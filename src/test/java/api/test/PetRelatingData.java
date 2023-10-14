a306e7bf27bff4e642a5b47ae2f3e7337b6ef291
Khaja Mohammed - Kairos <khaja.m@kairostech.com>
2023-10-14 16:08:19 +0530

    commit msg

---
+++ src/test/java/api/test/PetRelatingData.java
@@ -0,0 +1,12 @@
+package api.test;
+
+import org.testng.annotations.Test;
+
+public class PetRelatingData {
+	@Test
+	public void testExecution()
+	{
+		System.out.println("this is for updating the git hub localrepo");
+	}
+
+}
